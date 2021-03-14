d5cb2c7ef21b5a018a9d31a09787b476c9137f56
HangNT164 <hangnt16499@gmail.com>
2021-03-14 18:08:55 +0700

    Start project

---
+++ src/main/java/com/hangnt/timekeeping/main/TimekeepingServerApplication.java
@@ -0,0 +1,13 @@
+package com.hangnt.timekeeping.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimekeepingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangnt/timekeeping/main/TimekeepingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangnt.timekeeping.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimekeepingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
